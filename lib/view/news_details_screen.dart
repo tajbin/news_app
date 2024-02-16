4cfbb4ffd4672245a1483317fb7fec371fd4e178
tajbin <tajbin2013@gmail.com>
2024-02-16 19:22:31 +0600

    tajbin uodate

--- lib/view/news_details_screen.dart
+++ lib/view/news_details_screen.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+//tajbin
 
 class NewsDetailsScreen extends StatefulWidget {
   final String newImage, newsTitle, newsDate, author, description, content, source;
